11d740c4052df2a85eb208516f2a056c8efb88fd
Mostafa Negim <mostafa.negim@gmail.com>
2022-07-21 20:12:35 +0430

    Add: Employer

--- call_center/call_center.py
+++ call_center/call_center.py
@@ -1,6 +1,45 @@
-from enum import  Enum
+from enum import Enum
+from abc import ABCMeta, abstractmethod
+
+
 class Rank(Enum):
     OPERATOR = 0
     SUPERVISOR = 1
     DIRECTOR = 2
 
+
+class Employer(metaclass=ABCMeta):
+    def __init__(self, employee_id, name, rank, call_center):
+        self.employee_id = employee_id
+        self.name = name
+        self.rank = rank
+        self.call = None
+        self.call_center = call_center
+
+    def take_call(self, call):
+        """Assume"""
+        self.call = call
+        self.call.employee = self
+        self.call.state = CallState.IN_PROGRESS
+
+    def complete_ccall(self):
+        self.call.state = CallState.COMPLETE
+        self.call_center.notify_call_completed(self.call)
+
+    @abstractmethod
+    def escalate_call(self):
+        pass
+
+    def _escalate_call(self):
+        self.call.state = CallState.READY
+        call = self.call
+        self.call = None
+        self.call_center.notify_call_completed(call)
+
+
+
+
+class CallState(Enum):
+    READY = 0
+    IN_PROGRESS = 1
+    COMPLETE = 2
