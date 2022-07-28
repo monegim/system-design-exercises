87456e0b2ae6169e8aef955b561fbb3c3ae6e5d1
Mostafa Negim <mostafa.negim@gmail.com>
2022-07-28 17:17:08 +0430

    Add Contact class

--- python-object-oriented-programming/objects/objects-are-alike/extending-built-in.py
+++ python-object-oriented-programming/objects/objects-are-alike/extending-built-in.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from operator import concat
 from typing import List
 
 class ContactList(List["Contact"]):
@@ -10,4 +11,14 @@ class ContactList(List["Contact"]):
         return matching_contacts
 
 class Contact:
-    pass
+    all_contacts = ContactList()
+
+    def __init__(self, name: str, email: str) -> None:
+        self.name = name
+        self.email = email
+        Contact.all_contacts.append(self)
+    def __repr__(self) -> str:
+        return (
+        f"{self.__class__.__name__}("
+        f"{self.name!r}, {self.email!r}" f")"
+)
