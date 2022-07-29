3060ad75463eb277c2122ed86fc37ba3cb2c3fbe
Mostafa Negim <mostafa.negim@gmail.com>
2022-07-29 15:08:13 +0430

    Add NewReloaderCommand func

--- reloader/internal/pkg/cmd/reloader.go
+++ reloader/internal/pkg/cmd/reloader.go
@@ -1,7 +1,25 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 func NewReloaderCommand() *cobra.Command {
-	return &cobra.Command{}
+	cmd := &cobra.Command{
+		Use:     "reloader",
+		Short:   "A watcher for your Kubernetes cluster",
+		PreRunE: validateFlags,
+		Run:     startReloader,
+	}
+	return cmd
 }
+
+func validateFlags(*cobra.Command, []string) error {
+	err := fmt.Sprintf(("must be one of: "))
+	return errors.New(err)
+}
+
+func startReloader(cmd *cobra.Command, args []string) {}
