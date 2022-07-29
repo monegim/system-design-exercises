1f05fd2ebe5cfb57afa54b3280ee02f61d8de897
Mostafa Negim <mostafa.negim@gmail.com>
2022-07-29 13:51:13 +0430

    Add MD class

--- python-object-oriented-programming/objects/objects-are-alike/model.py
+++ python-object-oriented-programming/objects/objects-are-alike/model.py
@@ -12,6 +12,7 @@ class Sample:
         self.sepal_width = sepal_width
         self.petal_length = petal_length
         self.petal_width = petal_width
+
     def __repr__(self) -> str:
         return (
             f"{self.__class__.__name__}"
@@ -22,9 +23,11 @@ class Sample:
             f")"
         )
 
+
 class Distance:
     def distance(self, s1: Sample, s2: Sample) -> float:
         pass
+
 
 class ED(Distance):
     def distance(self, s1: Sample, s2: Sample) -> float:
@@ -33,4 +36,16 @@ class ED(Distance):
             s1.sepal_width - s2.sepal_width,
             s1.petal_length - s2.petal_length,
             s1.petal_width - s2.petal_widt
+        )
+
+
+class MD(Distance):
+    def distance(self, s1: Sample, s2: Sample) -> float:
+        return sum(
+            [
+            abs(s1.sepal_length - s2.sepal_length),
+            abs(s1.sepal_width - s2.sepal_width),
+            abs(s1.petal_length - s2.petal_length),
+            abs(s1.petal_width - s2.petal_widt)
+            ]
         )
