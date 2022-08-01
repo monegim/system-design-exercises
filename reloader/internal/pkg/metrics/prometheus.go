25b4aebe9780e83652fc204563571e361b7523b4
Mostafa Negim <mostafa.negim@gmail.com>
2022-08-01 21:02:55 +0430

    Add NewCollectors

--- reloader/internal/pkg/metrics/prometheus.go
+++ reloader/internal/pkg/metrics/prometheus.go
@@ -5,3 +5,20 @@ import "github.com/prometheus/client_golang/prometheus"
 type Collectors struct {
 	Reloader *prometheus.CounterVec
 }
+
+func NewCollectors() Collectors {
+
+	reloaded := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "reloader",
+			Name:      "reload_executed_total",
+			Help:      "Counter of reloads executed by Reloader.",
+		},
+		[]string{"success"},
+	)
+	reloaded.With(prometheus.Labels{"success": "true"}).Add(0)
+	reloaded.With(prometheus.Labels{"success": "false"}).Add(0)
+	return Collectors{
+		Reloader: reloaded,
+	}
+}
