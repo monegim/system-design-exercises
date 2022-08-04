2ce5bbf6db922529d410c4c79ed8dd7d475deda4
Mostafa Negim <mostafa.negim@gmail.com>
2022-08-04 11:41:49 +0430

    Update: supercronic

--- supercronic/main.go
+++ supercronic/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -14,7 +16,7 @@ func main() {
 		"prometheus-listen-address",
 		"",
 		fmt.Sprintf("give a valid ip[:port] address to expose Prometheus metrics at /metrics (port defaults to %s), "+
-		"use 0.0.0.0 for all network interfaces.", prometheus_metrics.DefaultPort),
+			"use 0.0.0.0 for all network interfaces.", prometheus_metrics.DefaultPort),
 	)
 	splitLogs := flag.Bool("split-logs", false, "split log output into stdout/stderr")
 	passthroughLogs := flag.Bool("passthrough-logs", false, "passthrough logs from commands, do not wrap them in Supercronic logging")
@@ -22,4 +24,29 @@ func main() {
 	sentryAlias := flag.String("sentryDsn", "", "alias for sentry-dsn")
 	overlapping := flag.Bool("overlapping", false, "enable tasks overlapping")
 	flag.Parse()
+
+	var sentryDsn string
+
+	sentryDsn = os.Getenv("SENTRY_DSN")
+
+	if *sentryAlias != "" {
+		sentryDsn = *sentryAlias
+	}
+	if *sentry != "" {
+		sentryDsn = *sentry
+	}
+	if *debug {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+	if *quiet {
+		logrus.SetLevel(logrus.WarnLevel)
+	}
+	if *json {
+		logrus.SetFormatter(&logrus.JSONFormatter{})
+	} else {
+		logrus.SetFormatter(&logrus.TextFormatter{FullTimestamp: true})
+	}
+	if *splitLogs {
+		
+	}
 }
