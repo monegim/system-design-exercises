5878478210551247d429b31794644734a8f2be79
Mostafa Negim <mostafa.negim@gmail.com>
2022-08-04 20:24:19 +0430

    feat(server.go): add NewExampleServer

--- cloud-native-go/design-patterns/circuit-breaker/example/server.go
+++ cloud-native-go/design-patterns/circuit-breaker/example/server.go
@@ -1,9 +1,20 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+)
 
 type ExampleServer struct {
 	addr      string
 	logger    *log.Logger
 	isEnabled bool
+}
+
+func NewExampleServer(addr string) *ExampleServer {
+	return &ExampleServer{
+		addr: addr,
+		logger: log.New(os.Stdout, "Server\t", log.LstdFlags),
+		isEnabled: true,
+	}
 }
