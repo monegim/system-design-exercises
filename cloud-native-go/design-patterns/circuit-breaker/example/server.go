f0e92a56440410abe72d09192a67c535fc2f566c
Mostafa Negim <mostafa.negim@gmail.com>
2022-08-04 20:46:02 +0430

    feat(sever.go) ListenAndServer()

--- cloud-native-go/design-patterns/circuit-breaker/example/server.go
+++ cloud-native-go/design-patterns/circuit-breaker/example/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -17,4 +18,22 @@ func NewExampleServer(addr string) *ExampleServer {
 		logger: log.New(os.Stdout, "Server\t", log.LstdFlags),
 		isEnabled: true,
 	}
+}
+
+func (s *ExampleServer) ListenAndServer() error {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if s.isEnabled {
+			s.logger.Println("responded with OK")
+			w.WriteHeader(http.StatusOK)
+		} else {
+			s.logger.Println("responded with Error")
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	})
+	http.HandleFunc("/toggle", func(w http.ResponseWriter, r *http.Request) {
+		s.isEnabled = !s.isEnabled
+		s.logger.Println("toggled. Is enagled:", s.isEnabled)
+		w.WriteHeader(http.StatusOK)
+	})
+	return http.ListenAndServe(s.addr, nil)
 }
