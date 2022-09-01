37aadf26bf5f2f8e4ae82939d0300add5a857744
Mostafa Negim <mostafa.negim@gmail.com>
2022-09-01 20:48:43 +0430

    feat: add Get func

--- cloud-native-go/ch05/core.go
+++ cloud-native-go/ch05/core.go
@@ -1,6 +1,18 @@
 package main
 
+import "errors"
+
 var store = make(map[string]string)
+
+var ErrorNoSuchKey = errors.New("no such key")
+
+func Get(key string) (string, error) {
+	value, ok := store[key]
+	if !ok {
+		return "", ErrorNoSuchKey
+	}
+	return value, nil
+}
 
 func Put(key string, value string) error {
 	store[key] = value
