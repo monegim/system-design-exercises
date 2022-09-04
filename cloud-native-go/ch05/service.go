e1c6aea27a78aba3944fcd6bc69599f774d2ebaf
Mostafa Negim <mostafa.negim@gmail.com>
2022-09-04 19:35:02 +0430

    feat: add loggingMiddleware func

--- cloud-native-go/ch05/service.go
+++ cloud-native-go/ch05/service.go
@@ -1,5 +1,20 @@
 package main
-import "github.com/gorilla/mux"
-func main(){
-	r := mux.NewRoute()
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
+func main() {
+	r := mux.NewRouter()
+
+	r.Use(loggingMiddleware)
 }
