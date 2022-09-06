9382bcb5c13c4cf289f5cc22cf0b2331ba3b0a2e
Mostafa Negim <mostafa.negim@gmail.com>
2022-09-06 18:44:44 +0430

    feat: add keyValueGetHandler:

--- cloud-native-go/ch05/service.go
+++ cloud-native-go/ch05/service.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
 
@@ -13,8 +14,25 @@ func loggingMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := Get(key)
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err!= nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte(value))
+	log.Printf("GET key=%s\n", key)
+}
 func main() {
 	r := mux.NewRouter()
 
 	r.Use(loggingMiddleware)
+	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
 }
