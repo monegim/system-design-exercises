c739cf65707b3ab7d860b2b22f167659981d673c
Mostafa Negim <mostafa.negim@gmail.com>
2022-10-26 12:41:48 +0330

    add cli package

---
+++ gitlab-ci-pipelines-exporter/internal/cli/cli.go
@@ -0,0 +1,14 @@
+package cli
+
+import (
+	"time"
+	"github.com/urfave/cli/v2"
+)
+
+func Run(version string, args []string) {
+	// err :=
+}
+
+func NewApp(version string, start time.Time) (app *cli.App) {
+	app = cli.NewApp()
+}
