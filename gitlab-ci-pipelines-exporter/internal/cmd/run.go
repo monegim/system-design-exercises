e6215402ebe50229ae786bd80b98bcee0aecadd5
Mostafa Negim <mostafa.negim@gmail.com>
2022-10-26 21:55:05 +0330

    add configure

--- gitlab-ci-pipelines-exporter/internal/cmd/run.go
+++ gitlab-ci-pipelines-exporter/internal/cmd/run.go
@@ -2,7 +2,11 @@ package cmd
 
 import "github.com/urfave/cli/v2"
 
-
-func Run(cliCtx *cli.Context)(int, error) {
+func Run(cliCtx *cli.Context) (int, error) {
+	cfg, err := configure(cliCtx)
+	if err != nil {
+		return 1, err
+	}
+	_ = cfg
 	return 0, nil
 }
