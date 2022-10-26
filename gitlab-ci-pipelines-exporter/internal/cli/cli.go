b368bc174951c82fa20b6810133086cb55d2382a
Mostafa Negim <mostafa.negim@gmail.com>
2022-10-26 18:39:18 +0330

    add Run func

--- gitlab-ci-pipelines-exporter/internal/cli/cli.go
+++ gitlab-ci-pipelines-exporter/internal/cli/cli.go
@@ -1,14 +1,37 @@
 package cli
 
 import (
+	"fmt"
+	"os"
 	"time"
+
 	"github.com/urfave/cli/v2"
 )
 
 func Run(version string, args []string) {
-	// err :=
+	err := NewApp(version, time.Now()).Run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func NewApp(version string, start time.Time) (app *cli.App) {
 	app = cli.NewApp()
+	app.Name = "gitlab-ci-pipelines-exporter"
+	app.Version = version
+	app.Usage = "Export metrics about Gitlab CI pipelines statuses"
+	app.EnableBashCompletion = true
+	app.Flags = cli.FlagsByName{
+		&cli.StringFlag{
+			Name:    "internal-monitoring-listener-address",
+			Aliases: []string{"m"},
+			EnvVars: []string{"GCPE_INTERNAL_MONITORING_LISTENER_ADDRESS"},
+			Usage:   "internal monitoring listener address",
+		},
+	}
+	app.Commands = cli.CommandsByName{
+
+	}
+	return
 }
