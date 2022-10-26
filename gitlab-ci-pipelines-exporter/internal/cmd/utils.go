1b54738b2c03b99267b47eb8e15f3a87f2bea2ff
Mostafa Negim <mostafa.negim@gmail.com>
2022-10-26 19:07:30 +0330

    add exitand ExecWrapper funcs

--- gitlab-ci-pipelines-exporter/internal/cmd/utils.go
+++ gitlab-ci-pipelines-exporter/internal/cmd/utils.go
@@ -1,6 +1,26 @@
 package cmd
 
-import "time"
+import (
+	"time"
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+)
 
 var start time.Time
 
+func exit(exitCode int, err error) cli.ExitCoder{
+	defer log.WithFields(
+		log.Fields{
+			"execution-time": time.Since(start),
+		},
+	).Debug("exited..")
+	if err != nil {
+		log.WithError(err).Error()
+	}
+	return cli.Exit("", exitCode)
+}
+func ExecWrapper(f func (ctx *cli.Context)(int, error)) cli.ActionFunc {
+	return func(ctx *cli.Context) error {
+		return exit(f(ctx))
+	}
+}
