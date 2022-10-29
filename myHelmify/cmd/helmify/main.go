54c6724bbec49df2e81fc68924b6c72475321d5e
Mostafa Negim <mostafa.negim@gmail.com>
2022-10-29 08:59:43 +0330

    update: main.go

--- myHelmify/cmd/helmify/main.go
+++ myHelmify/cmd/helmify/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	conf := ReadFlags()
 	stat, err := os.Stdin.Stat()
 	if err != nil {
 		logrus.WithError(err).Error("stdin error")
 		os.Exit(1)
 	}
-	_ = stat
+	_ = conf
+	fmt.Println(stat.Mode())
 }
