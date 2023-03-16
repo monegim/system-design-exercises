eb2b3641847cc904a3a4d4d3ec6ee4f97b6034b9
Mostafa Negim <mostafa.negim@gmail.com>
2023-03-16 14:33:57 +0330

    add all

--- retry/retry.go
+++ retry/retry.go
@@ -4,4 +4,24 @@ import "context"
 
 type RetryFunc func(ctx context.Context) error
 
-func Do(ctx context.Context, b backoff, f RetryFunc) error
+type retryableError struct {
+	err error
+}
+
+
+
+func Do(ctx context.Context, b Backoff, f RetryFunc) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+		}
+
+		err := f(ctx)
+		if err == nil {
+			return nil
+		}
+
+	}
+}
