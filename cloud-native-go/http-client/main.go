1de0bb0a853f36bdbde5b08f5beb3cb3b125b7ef
Mostafa Negim <mostafa.negim@gmail.com>
2023-03-17 13:33:39 +0330

    add header to the request

--- cloud-native-go/http-client/main.go
+++ cloud-native-go/http-client/main.go
@@ -11,12 +11,18 @@ func main() {
 	c := http.Client{
 		Timeout: time.Duration(2) * time.Second,
 	}
-	resp , err := c.Get("http://google.com")
+	req, err := http.NewRequest("GET", "https://example.com", nil)
 	if err != nil {
-		fmt.Printf("Error %s", err)
+		fmt.Printf("error %s", err)
+		return
+	}
+	req.Header.Add("Accept",`application/json`)
+	resp, err := c.Do(req)
+	if err != nil {
+		fmt.Printf("error %s", err)
 		return
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Printf("Body : %s", body)
+	fmt.Printf("Body: %s", body)
 }
