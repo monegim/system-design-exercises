3ee2856e846b6879414ff92aa28792244d0e39fe
Mostafa Negim <mostafa.negim@gmail.com>
2023-03-17 12:54:00 +0330

    simple get google.com request

--- cloud-native-go/http-client/main.go
+++ cloud-native-go/http-client/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
 func main() {
-	
+	c := http.Client{
+		Timeout: time.Duration(2) * time.Second,
+	}
+	resp , err := c.Get("http://google.com")
+	if err != nil {
+		fmt.Printf("Error %s", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Printf("Body : %s", body)
 }
