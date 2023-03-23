20666c8591eeaf0e67fff5ff50f597fee50bba04
Mostafa Negim <mostafa.negim@gmail.com>
2023-03-23 22:55:19 +0430

    Blocking channel

--- cloud-native-go/chan/main.go
+++ cloud-native-go/chan/main.go
@@ -7,19 +7,21 @@ import (
 
 func main() {
 	ch := make(chan int)
-	fmt.Println("Sending value to channel")
 	go send(ch)
 
-	fmt.Println("Receiving from channel")
 	go receive(ch)
-	time.Sleep(1 * time.Second)
+	time.Sleep(2 * time.Second)
 }
 
 func send(ch chan int) {
 	ch <- 1
+	fmt.Println("Sending value to channel complete")
 }
 
-func receive(ch chan int)  {
-	val := <- ch
-	fmt.Printf("Value Received=%d in receive function\n", val)
+func receive(ch chan int) {
+	time.Sleep(1 * time.Second)
+	fmt.Println("Timeout finished")
+
+	_ = <- ch
+	return
 }
