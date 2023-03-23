cc450ba860c41e4f92d6d1edbdba9d9fa3040651
Mostafa Negim <mostafa.negim@gmail.com>
2023-03-23 22:04:07 +0430

    Receive Operation

--- cloud-native-go/chan/main.go
+++ cloud-native-go/chan/main.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	// var a chan int
-	a := make(chan int)
-	fmt.Println(a)
+	ch := make(chan int)
+	fmt.Println("Sending value to channel")
+	go send(ch)
+
+	fmt.Println("Receiving from channel")
+	go receive(ch)
+	time.Sleep(1 * time.Second)
+}
+
+func send(ch chan int) {
+	ch <- 1
+}
+
+func receive(ch chan int)  {
+	val := <- ch
+	fmt.Printf("Value Received=%d in receive function\n", val)
 }
