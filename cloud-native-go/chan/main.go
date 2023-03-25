6a55316aadf0d45a57f4c4f5b5f8cc26ec229266
Mostafa Negim <mostafa.negim@gmail.com>
2023-03-25 22:19:26 +0430

    dead lock

--- cloud-native-go/chan/main.go
+++ cloud-native-go/chan/main.go
@@ -2,26 +2,13 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	ch := make(chan int)
-	go send(ch)
-
-	go receive(ch)
-	time.Sleep(2 * time.Second)
-}
-
-func send(ch chan int) {
+	ch := make(chan int, 1)
 	ch <- 1
+	ch <- 2
 	fmt.Println("Sending value to channel complete")
-}
-
-func receive(ch chan int) {
-	time.Sleep(1 * time.Second)
-	fmt.Println("Timeout finished")
-
-	_ = <- ch
-	return
+	val := <-ch
+	fmt.Printf("Value received: %d\n", val)
 }
