63d95fba55cbc3f515c505293d03363ae76cee4b
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-01 23:53:03 +0430

    add getHello method

--- cloud-native-go/http-server/main.go
+++ cloud-native-go/http-server/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/",getRoot)
+	http.HandleFunc("/hello",getHello)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		os.Exit(1)
@@ -18,4 +19,9 @@ func main() {
 func getRoot(w http.ResponseWriter, r *http.Request)  {
 	fmt.Printf("got / request\n")
 	io.WriteString(w, "This is my website!\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request)  {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello Http\n")
 }
