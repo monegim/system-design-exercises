1fbcc64d1edd8b9f2dc08e24f4265288530c6da8
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-01 23:55:05 +0430

    add custom error

--- cloud-native-go/http-server/main.go
+++ cloud-native-go/http-server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -11,7 +12,10 @@ func main() {
 	http.HandleFunc("/",getRoot)
 	http.HandleFunc("/hello",getHello)
 	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
+	if errors.Is(err, http.ErrServerClosed){
+		fmt.Printf("server closed\n")
+	} else	if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
 		os.Exit(1)
 	}
 }
