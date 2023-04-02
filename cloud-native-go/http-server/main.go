effe531a502afdab0d13f3f831fa0b5014174bfc
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-03 00:31:31 +0430

    add support for multiple server

--- cloud-native-go/http-server/main.go
+++ cloud-native-go/http-server/main.go
@@ -1,32 +1,67 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
-	"os"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/",getRoot)
-	mux.HandleFunc("/hello",getHello)
-	err := http.ListenAndServe(":8080", mux)
-	if errors.Is(err, http.ErrServerClosed){
-		fmt.Printf("server closed\n")
-	} else	if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
-		os.Exit(1)
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	serverOne := &http.Server{
+		Addr:    ":33333",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
 	}
+	serverTwo := &http.Server{
+		Addr:    ":4444",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+	go func() {
+		err := serverOne.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server one closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server one: %s\n", err)
+		}
+		cancelCtx()
+	}()
+	go func() {
+		err := serverTwo.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server two closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server two: %s\n", err)
+		}
+		cancelCtx()
+	}()
+	<-ctx.Done()
+
 }
 
-func getRoot(w http.ResponseWriter, r *http.Request)  {
-	fmt.Printf("got / request\n")
+const keyServerAddr = "serverAddr"
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	fmt.Printf("%s: got / request\n", ctx.Value(keyServerAddr))
 	io.WriteString(w, "This is my website!\n")
 }
 
-func getHello(w http.ResponseWriter, r *http.Request)  {
-	fmt.Printf("got /hello request\n")
+func getHello(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
 	io.WriteString(w, "Hello Http\n")
 }
