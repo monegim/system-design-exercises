16dcbb428cb3adc95c23e9c0d29915474c989ca0
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-04 01:49:11 +0430

    add support for form

--- cloud-native-go/http-server/main.go
+++ cloud-native-go/http-server/main.go
@@ -13,6 +13,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
 	ctx := context.Background()
 	server := &http.Server{
 		Addr:    ":3333",
@@ -51,8 +52,12 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "This is my website!\n")
 }
 
-// func getHello(w http.ResponseWriter, r *http.Request) {
-// 	ctx := r.Context()
-// 	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
-// 	io.WriteString(w, "Hello Http\n")
-// }
+func getHello(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
+	myName := r.PostFormValue("myName")
+	if myName == "" {
+		myName = "HTTP"
+	}
+	io.WriteString(w, fmt.Sprintf("Hello, %s!\n", myName))
+}
