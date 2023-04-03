6d136c74c8d25bf9f95cfaf90ded6eb540552109
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-04 01:52:43 +0430

    add support for header

--- cloud-native-go/http-server/main.go
+++ cloud-native-go/http-server/main.go
@@ -57,7 +57,9 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
 	myName := r.PostFormValue("myName")
 	if myName == "" {
-		myName = "HTTP"
+		w.Header().Set("x-missing-field", "myName")
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 	io.WriteString(w, fmt.Sprintf("Hello, %s!\n", myName))
 }
